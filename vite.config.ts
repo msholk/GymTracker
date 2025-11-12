f0fbf75ff8565e76abffb13b5d4cbe929706ffbf
AzureAD\MaxSholk <Max.Sholk@infonis.com>
2025-11-12 10:17:19 +0100

    installable

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,51 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'GymTracker',
+                short_name: 'GymTracker',
+                start_url: '.',
+                display: 'standalone',
+                background_color: '#ffffff',
+                theme_color: '#4F8A8B',
+                icons: [
+                    {
+                        src: 'assets/icon-192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'assets/icon-512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            },
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+                runtimeCaching: [
+                    {
+                        urlPattern: /^https:\/\/.*/, // cache all network requests
+                        handler: 'NetworkFirst',
+                        options: {
+                            cacheName: 'external-cache',
+                            expiration: {
+                                maxEntries: 50,
+                                maxAgeSeconds: 60 * 60 * 24 * 30 // 30 Days
+                            }
+                        }
+                    }
+                ]
+            }
+        })
+    ],
     build: {
         outDir: 'public',
         emptyOutDir: true
